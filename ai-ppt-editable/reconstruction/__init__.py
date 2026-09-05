43507fd883482750f303d22e1f4dc251d94d07cb
joeshu <350027830@qq.com>
2026-09-05 11:28:12 +0800

    feat(reconstruction): export quality gate contracts

--- ai-ppt-editable/reconstruction/__init__.py
+++ ai-ppt-editable/reconstruction/__init__.py
@@ -3,6 +3,7 @@
 from .graph_ir import PageGraph, GraphValidationError
 from .difference_graph import DifferenceGraph
 from .repair_router import RepairRouter, RepairPlan
+from .quality_gate import QualityGate, QualityThresholds, GateResult
 
 __all__ = [
     "PageGraph",
@@ -10,4 +11,7 @@ __all__ = [
     "DifferenceGraph",
     "RepairRouter",
     "RepairPlan",
+    "QualityGate",
+    "QualityThresholds",
+    "GateResult",
 ]
