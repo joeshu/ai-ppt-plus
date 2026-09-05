94ab97f0bae340a85e2d6b76c0c831d33b7bfd08
joeshu <350027830@qq.com>
2026-09-06 03:05:56 +0800

    feat: export fidelity policy and screenshot text target contracts

--- ai-ppt-editable/reconstruction/__init__.py
+++ ai-ppt-editable/reconstruction/__init__.py
@@ -4,11 +4,13 @@ from .graph_ir import PageGraph, GraphValidationError
 from .difference_graph import DifferenceGraph
 from .repair_router import RepairRouter, RepairPlan
 from .quality_gate import QualityGate, QualityThresholds, GateResult
+from .quality_policy import FidelityPolicy, DEFAULT_POLICY, POLICY_VERSION
 from .pipeline import ReconstructionPipeline, PipelineState, Stage
 from .manifest_bridge import build_page_graph
 from .repair_executors import RepairExecutionError, execute_action, execute_plan
 from .source_coverage import audit_source_coverage, extract_pptx_objects
 from .typography_search import calibrate_typography, measurement_loss
+from .text_target_spec import build_text_target_spec
 from .relation_geometry import solve_peer_layout, solve_graph_relations
 from .asset_subject import subject_placement
 from .asset_metrics import compare_asset_subjects
@@ -31,6 +33,9 @@ __all__ = [
     "QualityGate",
     "QualityThresholds",
     "GateResult",
+    "FidelityPolicy",
+    "DEFAULT_POLICY",
+    "POLICY_VERSION",
     "ReconstructionPipeline",
     "PipelineState",
     "Stage",
@@ -42,6 +47,7 @@ __all__ = [
     "extract_pptx_objects",
     "calibrate_typography",
     "measurement_loss",
+    "build_text_target_spec",
     "solve_peer_layout",
     "solve_graph_relations",
     "subject_placement",
