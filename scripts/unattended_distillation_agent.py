bf23a61eec38e8cc4dd44418203433de9a659927
joeshu <350027830@qq.com>
2026-09-18 11:56:55 +0800

    refactor: remove retired distillation agent tombstone

--- scripts/unattended_distillation_agent.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Compatibility tombstone for the retired distillation subsystem."""
-import sys
-
-print("unattended distillation has been retired; use the normal deterministic QA/Repair Trace workflow", file=sys.stderr)
-raise SystemExit(2)
